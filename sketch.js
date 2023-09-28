e20e5f69c68c787cc1fc775f892c3310e7c56eba
Lostthetige <59176569+Lostthetige@users.noreply.github.com>
2023-09-28 12:45:48 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+let zBereich; 
+
+
+
+function setup() {
+ zBereich = createCanvas(windowWidth, windowHeight);
+
+
+zBereich.position(0, 0);
+zBereich.style('z-index','-1'); 
+
+}
+
+
+function draw() {
+noStroke();
+fill(255, 55);
+circle(mouseX, mouseY, 24);
+}
+
+// function mousePressed (  ) {
+// background(0);
+  
+// }
